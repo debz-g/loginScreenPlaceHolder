36035a4748b1e734b1e0b37a5b733382882c833a
debz-g <debz.exe@gmail.com>
2022-08-30 21:27:20 +0530

    initial and final commit for login-signup placeholder

---
+++ app/src/main/java/dev/redfox/easyplacement/MainActivity.kt
@@ -0,0 +1,11 @@
+    package dev.redfox.easyplacement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
